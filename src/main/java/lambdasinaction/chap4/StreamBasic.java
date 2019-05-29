7b04bb8a026949e669d7a50ea33639f40d174db4
zongzhankui <zongzhankui@hotmail.com>
2019-05-29 20:44:13 +0800

    stream

--- src/main/java/lambdasinaction/chap4/StreamBasic.java
+++ src/main/java/lambdasinaction/chap4/StreamBasic.java
@@ -1,16 +1,16 @@
 package lambdasinaction.chap4;
 
-import java.util.*;
-import java.util.stream.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.toList;
 
-import static lambdasinaction.chap4.Dish.menu;
-
 public class StreamBasic {
 
-    public static void main(String...args){
+    public static void main(String... args) {
         // Java 7
         getLowCaloricDishesNamesInJava7(Dish.menu).forEach(System.out::println);
 
@@ -21,26 +21,27 @@ public class StreamBasic {
 
     }
 
-    public static List<String> getLowCaloricDishesNamesInJava7(List<Dish> dishes){
+    public static List<String> getLowCaloricDishesNamesInJava7(List<Dish> dishes) {
         List<Dish> lowCaloricDishes = new ArrayList<>();
-        for(Dish d: dishes){
-            if(d.getCalories() < 400){
+        for (Dish d : dishes) {
+            if (d.getCalories() < 400) {
                 lowCaloricDishes.add(d);
             }
         }
         List<String> lowCaloricDishesName = new ArrayList<>();
         Collections.sort(lowCaloricDishes, new Comparator<Dish>() {
-            public int compare(Dish d1, Dish d2){
+            @Override
+            public int compare(Dish d1, Dish d2) {
                 return Integer.compare(d1.getCalories(), d2.getCalories());
             }
         });
-        for(Dish d: lowCaloricDishes){
+        for (Dish d : lowCaloricDishes) {
             lowCaloricDishesName.add(d.getName());
         }
         return lowCaloricDishesName;
     }
 
-    public static List<String> getLowCaloricDishesNamesInJava8(List<Dish> dishes){
+    public static List<String> getLowCaloricDishesNamesInJava8(List<Dish> dishes) {
         return dishes.stream()
                 .filter(d -> d.getCalories() < 400)
                 .sorted(comparing(Dish::getCalories))
